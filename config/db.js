d58343da06db713c9af686d0af202e9460d0f2c3
Marko Jurcic <Marko.Jurcic@hudsonhomes.com.au>
2019-09-10 16:53:36 +1000

    Removed API files

--- config/db.js
+++
@@ -1,4 +0,0 @@
-database = "mongodb+srv://iparkserv:hfKmZQWeNz0LP9UO@cluster0-5wctp.mongodb.net/test?retryWrites=true&w=majority"
-module.exports = {
-    mongoURI: database
-}
--- config/sendgrid.js
+++
@@ -1,5 +0,0 @@
-sendgridAPI='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'
-
-module.exports = {
-    sendgridURI: sendgridAPI
-}
